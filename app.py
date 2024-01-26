fbba49a387eb32cff4ecc162ab3c152f6d49920a
LetíciaCastelo <leticiavcastelo@gmail.com>
2024-01-26 19:42:08 -0300

    first commit - app food

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from modelos.restaurante import Restaurante
 
 restaurante_president = Restaurante('Prèsident', 'Francês')
-restaurante_president.receber_avaliacao('Letícia', 10)
-restaurante_president.receber_avaliacao('Caroline', 9)
-restaurante_president.receber_avaliacao('Enio', 8)
+restaurante_president.receber_avaliacao('Letícia', 8)
+restaurante_president.receber_avaliacao('Caroline', 6)
+restaurante_president.receber_avaliacao('Enio', 4)
 
 def main():
   Restaurante.listar_restaurantes()
