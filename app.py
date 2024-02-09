e978bcc62076c503aca331979e59e9d18ffbb180
LetíciaCastelo <leticiavcastelo@gmail.com>
2024-02-09 11:27:42 -0300

    Atualizações

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 from modelos.restaurante import Restaurante
+from modelos.cardapio.bebida import Bebida
+from modelos.cardapio.prato import Prato
+from modelos.cardapio.sobremesa import Sobremesa
 
 restaurante_president = Restaurante('Prèsident', 'Francês')
-restaurante_president.receber_avaliacao('Letícia', 8)
-restaurante_president.receber_avaliacao('Caroline', 6)
-restaurante_president.receber_avaliacao('Enio', 4)
+bebida_suco = Bebida('Acerola', 5.0, 'Grande')
+bebida_suco.aplicar_desconto()
+prato_risoto = Prato('Risoto', 46.0, 'Risoto de Camarão')
+prato_risoto.aplicar_desconto()
+sobremesa_sorvete = Sobremesa('Sorvete de Pistache', 17.0, 'Sorvete', 'Grande')
+sobremesa_sorvete.aplicar_desconto()
+restaurante_president.adicionar_no_cardapio(bebida_suco)
+restaurante_president.adicionar_no_cardapio(prato_risoto)
+restaurante_president.adicionar_no_cardapio(sobremesa_sorvete)
 
 def main():
-  Restaurante.listar_restaurantes()
+  restaurante_president.exibir_cardapio
   
 if __name__ =='__main__':
   main()
+  
